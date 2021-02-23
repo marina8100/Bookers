3c2c5dddc7a0f5d764d58a06ee4eebf667e2cbf1
marina8100 <grapefriut.8100@gmail.com>
2021-02-23 12:55:14 +0000

    Update

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,4 +1,45 @@
 class BooksController < ApplicationController
-  def new
-  end
+  
+ def new
+  @book = Book.new
+ end
+  
+ def create
+  # １. データを新規登録するためのインスタンス作成
+  book = Book.new(book_params)
+  # ２. データをデータベースに保存するためのsaveメソッド実行
+  book.save
+  # ３. トップ画面へリダイレクト
+  redirect_to books_path(list.id)
+ end
+
+ def index
+  @books = Book.all
+ end
+
+ def show
+  @book = Book.find(params[:id])
+ end
+ 
+ def edit
+  @book = Book.find(params[:id])
+ end
+ 
+ def update
+  book = Book.find(params[:id])
+  book.update(book_params)
+  redirect_to books_path(book.id)
+ end
+ 
+ def destroy
+  book = Book.find(params[:id])  # データ（レコード）を1件取得
+  book.destroy  # データ（レコード）を削除
+  redirect_to books_path  # 投稿一覧画面へリダイレクト
+ end
+ 
+ private
+ # ストロングパラメータ
+ def book_params
+   params.require(:book).permit(:title, :body)
+ end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_02_07_134414) do
+ActiveRecord::Schema.define(version: 2021_02_15_130605) do
 
   create_table "books", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "title"
   end
 
 end
