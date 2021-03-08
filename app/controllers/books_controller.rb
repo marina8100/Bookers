ddd1842b4e523304e318f91ad38c1d8f97b5b2d5
marina8100 <grapefruit.8100@gmails.com>
2021-03-08 12:52:17 +0000

    Finish

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,16 +1,20 @@
 class BooksController < ApplicationController
-  
+
  def new
   @book = Book.new
  end
-  
+
  def create
   # １. データを新規登録するためのインスタンス作成
-  book = Book.new(book_params)
+  @new_book = Book.new(book_params)
   # ２. データをデータベースに保存するためのsaveメソッド実行
-  book.save
-  # ３. トップ画面へリダイレクト
-  redirect_to book_path(book.id)
+  if @new_book.save
+  # ３. 詳細画面へリダイレクト
+    redirect_to book_path(@new_book.id)
+  else
+    @books = Book.all
+    render "index"
+  end
  end
 
  def index
@@ -21,23 +25,26 @@ class BooksController < ApplicationController
  def show
   @book = Book.find(params[:id])
  end
- 
+
  def edit
   @book = Book.find(params[:id])
  end
- 
+
  def update
-  book = Book.find(params[:id])
-  book.update(book_params)
-  redirect_to book_path(book.id)
+  @book = Book.find(params[:id])
+  if @book.update(book_params)
+   redirect_to book_path(@book.id)
+  else
+   render "edit"
+  end
  end
- 
+
  def destroy
   book = Book.find(params[:id])  # データ（レコード）を1件取得
   book.destroy  # データ（レコード）を削除
   redirect_to books_path  # 投稿一覧画面へリダイレクト
  end
- 
+
  private
  # ストロングパラメータ
  def book_params
--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,7 @@
 Rails.application.routes.draw do
-  get 'books/new'
   root 'books#top'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get 'top' => 'books#top'
-  post 'books' => 'books#create'
-  get 'books' => 'books#index'
-  get 'books/:id' => 'books#show', as: 'book'
-  get 'books/:id/edit' => 'books#edit', as: 'edit_book'
-  patch 'books/:id' => 'books#update', as: 'update_book'
-  delete 'books/:id' => 'books#destroy', as: 'destroy_book'
+  resources :books
+  
 end
